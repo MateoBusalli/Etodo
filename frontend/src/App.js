989e0670a9a58bee087e1c1c0544d8452c131dca
Mateo <matb0407@gmail.com>
2025-11-04 13:53:00 +0100

    repo init

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <html>
+      <head>
+        <title>DoNext</title>
+      </head>
+      <body class="darkgrey-color">
+        <h2 class="center white_font">DoNext</h2>
+        <hr class="blue-color"/>
+        
+      </body>
+    </html>
+  );
+}
+
+export default App;
